3bd1ad65a9874b09d65ca3a56330d849849c8f8c
beard33 <andreaguzzon26@gmail.com>
2021-04-22 15:17:24 +0200

    Add source and test files

---
+++ skyscraper.hs
@@ -0,0 +1,46 @@
+import System.IO(readFile, IOMode(ReadMode), hGetContents, hClose, openFile)
+import Data.List (transpose)
+
+-- Check if the provided list is a valid skyscraper list
+-- (i.e. if values are unique and within the range)
+isListValid :: [Int] -> Bool
+isListValid list = allUnique' list && all (\x -> x <= length list && x > 0) list
+
+allUnique' :: [Int] -> Bool
+allUnique' [] = True
+allUnique' (x:xs) = x `notElem` xs && allUnique' xs
+
+
+-- Perform the rooftop count
+rooftops :: [Int] -> Int -> Int -> Bool
+rooftops [] highest count = count == 0
+rooftops (x : xs) highest count
+    | x > highest = rooftops xs x (count-1)
+    | otherwise  = rooftops xs highest count
+
+-- Check if the matrix is a valid solution
+-- for the skyscraper problem
+checkMatrix :: [[Int]] -> Bool
+checkMatrix [] = True
+checkMatrix ((0 : xs) : xss) = isListValid (init xs)
+                        && checkMatrix xss
+checkMatrix ((x : xs) : xss) = isListValid (init xs)
+                        && rooftops (init xs) 0 x
+                        && checkMatrix xss
+
+
+-- Parse line into a list of Int
+parseLine :: String -> [Int]
+parseLine line = map read $ words line :: [Int]
+
+main :: IO ()
+main = do
+    handle <- openFile "skyscrapers-6x6.txt" ReadMode
+    contents <- hGetContents handle
+    let allLines = lines contents
+    let matrix = map parseLine allLines
+    let matrix' = transpose matrix
+    let res = checkMatrix(tail (init matrix)) 
+            && checkMatrix(tail (init matrix'))
+    if res then print "Solution is correct" else print "Solution is wrong"
+    hClose handle
